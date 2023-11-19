e554da27d4fdd84a2d1e0fabeb78761e3b89d0af
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-11-19 15:25:48 +0100

    Refactorisation du nom EtapeFixture.php en StepFixture.php et ajout de createMany de StepFactory dans la methode load()

--- src/DataFixtures/StepFixtures.php
+++ src/DataFixtures/StepFixtures.php
@@ -2,16 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Step;
+use App\Factory\StepFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class EtapeFixtures extends Fixture
+class StepFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        StepFactory::createMany(10);
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
     }
 }

d938eef448105f3c646a294316bfd56ac117ba3f
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-11-20 11:59:25 +0100

    generation de la forge

--- src/DataFixtures/StoreFixtures.php
+++ src/DataFixtures/StoreFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\StoreFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,6 @@ class StoreFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        StoreFactory::createMany(10);
     }
 }

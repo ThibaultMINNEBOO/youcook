2c12ebc2024ad580284f57a9575d03d25eec882b
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-11-20 08:29:26 +0100

    migration effectuée

---
+++ migrations/Version20231120072450.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231120072450 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient CHANGE ingredient_name name VARCHAR(50) NOT NULL, CHANGE ingredient_picture picture VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient CHANGE name ingredient_name VARCHAR(50) NOT NULL, CHANGE picture ingredient_picture VARCHAR(255) DEFAULT NULL');
+    }
+}

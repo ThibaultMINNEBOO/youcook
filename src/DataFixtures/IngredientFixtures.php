2be7fcec720671d4afa37f09fd1603f609309d00
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-11-25 22:36:11 +0100

    feat: add dependency between IngredientCategory fixtures and Ingredient fixtures

--- src/DataFixtures/IngredientFixtures.php
+++ src/DataFixtures/IngredientFixtures.php
@@ -5,14 +5,22 @@ namespace App\DataFixtures;
 use App\Factory\IngredientCategoryFactory;
 use App\Factory\IngredientFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class IngredientFixtures extends Fixture
+class IngredientFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         IngredientFactory::createMany(20, [
             'category' => IngredientCategoryFactory::faker()->boolean(70) ?? IngredientCategoryFactory::random(),
         ]);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            IngredientCategoryFixtures::class,
+        ];
     }
 }

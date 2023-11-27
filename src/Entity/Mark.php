a3284eea3ccac4ce441995a508a4cdfa1b8d724d
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-11-27 12:01:16 +0100

    Ajout de l'attribut de relation user dans Mark

--- src/Entity/Mark.php
+++ src/Entity/Mark.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MarkRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -17,6 +19,14 @@ class Mark
     #[ORM\Column(type: Types::DECIMAL, precision: 1, scale: 1, nullable: true)]
     private ?string $mark = null;
 
+    #[ORM\OneToMany(mappedBy: 'mark', targetEntity: User::class)]
+    private Collection $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +40,36 @@ class Mark
     public function setMark(?string $mark): static
     {
         $this->mark = $mark;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): static
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setMark($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): static
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getMark() === $this) {
+                $user->setMark(null);
+            }
+        }
 
         return $this;
     }

9dac7be210bada6ce9057b9bebf2ee2bbd7b75f9
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-12-01 11:33:24 +0100

    Correction du RecipeFixture et ajout du getDependencies

--- src/DataFixtures/RecipeFixtures.php
+++ src/DataFixtures/RecipeFixtures.php
@@ -3,13 +3,25 @@
 namespace App\DataFixtures;
 
 use App\Factory\RecipeFactory;
+use App\Factory\RecipesCategoryFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class RecipeFixtures extends Fixture
+class RecipeFixtures extends Fixture implements DependentFixtureInterface
+
 {
     public function load(ObjectManager $manager): void
     {
-        RecipeFactory::createMany(20);
+        RecipeFactory::createMany(20, function () {
+            return ['recipeCategory' => RecipesCategoryFactory::random()];
+        });
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            RecipeCategoryFixtures::class,
+        ];
     }
 }

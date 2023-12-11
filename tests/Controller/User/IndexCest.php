0cb368808d6216a34b30b55f261e695be905b226
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-11 11:06:04 +0100

    test(profile): add test of user profile

--- tests/Controller/User/IndexCest.php
+++ tests/Controller/User/IndexCest.php
@@ -1,12 +1,25 @@
 <?php
 
-
 namespace App\Tests\Controller\User;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
 {
+    public function testShowProfileWhenAuthenticated(ControllerTester $I)
+    {
+        $user = UserFactory::createOne()->object();
+        $I->amLoggedInAs($user);
+
+        $I->amOnPage('/profile');
+
+        $I->seeInTitle("{$user->getFirstname()} | Profile");
+        $I->seeElement('form');
+
+        $I->seeResponseCodeIs(200);
+    }
+
     public function _before(ControllerTester $I)
     {
     }

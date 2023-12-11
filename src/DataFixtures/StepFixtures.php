faf9e1993087daf110db502528fe1a2c6ef49808
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2023-12-11 20:34:46 +0100

    ajout d'une dépendance de StepFixtures vers RecipeFixtures

--- src/DataFixtures/StepFixtures.php
+++ src/DataFixtures/StepFixtures.php
@@ -4,12 +4,20 @@ namespace App\DataFixtures;
 
 use App\Factory\StepFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class StepFixtures extends Fixture
+class StepFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         StepFactory::createMany(10);
+    }
+
+    public function getDependencies()
+    {
+        return [
+            RecipeFixtures::class,
+        ];
     }
 }

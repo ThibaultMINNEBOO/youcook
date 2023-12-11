a8b88b10be09f8df1dd891fe6b0613cdb65c5817
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-11 11:13:23 +0100

    feat(profile): allow user who is authenticated to access to his profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/profile', name: 'app_user_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = $userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
+
+        return $this->render('user/index.html.twig');
     }
 }

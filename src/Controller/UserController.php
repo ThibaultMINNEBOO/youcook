766e7cccf9d96427c2ad64ea694bb5a3f2843f88
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-12 22:35:12 +0100

    fix: change form import to custom UserType

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
-use Cassandra\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Form\UserType;
 
 class UserController extends AbstractController
 {

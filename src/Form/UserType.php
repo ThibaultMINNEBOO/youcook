0fca6a5eae9e24c376b795f4bee2606dc9b663d5
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-12 22:39:23 +0100

    fix: translate error messages into french

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -25,11 +25,11 @@ class UserType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),

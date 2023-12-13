e16b547938be20a3f8b2b2c6c8d4dd5999942c7a
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-13 09:11:05 +0100

    fix(profile): format file using php-cs-fixer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Form\ProfileType;
-use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;

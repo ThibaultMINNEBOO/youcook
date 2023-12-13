4dcb19bfce02b42b42532b48ee85bb123fa54e00
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-13 09:23:53 +0100

    feat(profile): add validation of form and redirect to login page
    TODO: When home page is done, change app_login to the route of home page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Form\ProfileType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,11 +15,18 @@ class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_user_profile')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
 
         $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
 
         return $this->render('user/index.html.twig', [
             'form' => $form->createView(),

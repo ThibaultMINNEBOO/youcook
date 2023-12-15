303f774c5e0d48a4b5b3f8728d91fd546a71c210
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-12-15 10:00:30 +0100

    ajout d'un bouton créer

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -8,6 +8,7 @@ use App\Entity\Tool;
 use App\Repository\ToolRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -33,6 +34,9 @@ class RecipeType extends AbstractType
                 'class' => RecipesCategory::class,
                 'choice_label' => 'name',
             ])
+            ->add('save', SubmitType::class,
+                ['label' => 'Créer'])
+
         ;
     }
 

ae76dbff86627893e9b471e02839da985b66b8d0
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2023-12-15 09:06:28 +0100

    création du controller et de la vue concernant la creation  de recette

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,11 @@ class RecipeController extends AbstractController
         return $this->render('recipe/index.html.twig', [
             'controller_name' => 'RecipeController',
         ]);
+    }
+
+    #[Route('/recipe/{id}/create', name: 'app_recipe_create', requirements: ['id' => '\d+'])]
+    public function create(User $user): Response
+    {
+        return $this->render('recipe/create.html.twig');
     }
 }

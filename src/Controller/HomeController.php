ed413c39c79c151f093ddb2d74ecb267271d0c0f
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-12-18 14:27:52 +0100

    refactor des nom Accueil en Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }

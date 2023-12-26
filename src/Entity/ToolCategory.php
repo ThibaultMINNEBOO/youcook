49d1fd2ef36bf180074f22721925da34b5dedcb9
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2023-12-26 14:47:08 +0100

    ajout de la relation entre les entités Tool et ToolCategory

--- src/Entity/ToolCategory.php
+++ src/Entity/ToolCategory.php
@@ -17,6 +17,13 @@ class ToolCategory
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ORM\OneToMany(mappedBy: 'toolCategory', targetEntity: Tool::class)]
+    private Collection $tools;
+
+    public function __construct()
+    {
+        $this->tools = new ArrayCollection();
+    }
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $description = null;
 
@@ -45,6 +52,36 @@ class ToolCategory
     public function setDescription(?string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tool>
+     */
+    public function getTools(): Collection
+    {
+        return $this->tools;
+    }
+
+    public function addTool(Tool $tool): static
+    {
+        if (!$this->tools->contains($tool)) {
+            $this->tools->add($tool);
+            $tool->setToolCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTool(Tool $tool): static
+    {
+        if ($this->tools->removeElement($tool)) {
+            // set the owning side to null (unless already changed)
+            if ($tool->getToolCategory() === $this) {
+                $tool->setToolCategory(null);
+            }
+        }
 
         return $this;
     }

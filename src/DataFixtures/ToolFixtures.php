13d26005f1f5d4296cccdfaad3ade4f755c5f338
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2023-12-26 15:12:47 +0100

    modification de ToolFixtures.php pour attribuer une catégorie existante ou de la créer

--- src/DataFixtures/ToolFixtures.php
+++ src/DataFixtures/ToolFixtures.php
@@ -2,14 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ToolCategoryFactory;
 use App\Factory\ToolFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ToolFixtures extends Fixture
+class ToolFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        ToolFactory::createMany(10);
+        $tools = json_decode(file_get_contents(__DIR__.'/data/Tool.json'), true);
+        foreach ($tools as $tool) {
+            ToolFactory::createOne([
+                'name' => $tool['name'],
+                'toolCategory' => ToolCategoryFactory::findOrCreate([
+                    'name' => $tool['toolCategory'],
+                ])]
+            );
+        }
+    }
+
+    public function getDependencies(): array
+    {
+        return [ToolCategoryFixtures::class];
     }
 }

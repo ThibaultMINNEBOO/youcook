86d0f8f18029afe93ada95fe6471467d72beffe6
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2023-12-26 15:23:22 +0100

    ajout de l'action de création d'outil

--- src/Controller/ToolController.php
+++ src/Controller/ToolController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Tool;
+use App\Entity\ToolCategory;
+use App\Form\ToolType;
 use App\Repository\ToolRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,4 +30,28 @@ class ToolController extends AbstractController
         return $this->render('tool/show.html.twig', ['tool' => $tool]);
     }
 
+    #[Route('/tool/create', name: 'app_tool_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $tool = new Tool();
+        $formTool = $this->createForm(ToolType::class, $tool);
+        $formTool->add('toolCategory', EntityType::class, [
+            'class' => ToolCategory::class,
+            'choice_label' => 'name',
+            'data' => $tool->getToolCategory(),
+        ]);
+
+
+        $formTool->handleRequest($request);
+        if ($formTool->isSubmitted() && $formTool->isValid()) {
+            $entityManager->persist($tool);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_tool_show', ['id' => $tool->getId()]);
+        }
+
+        return $this->render('tool/create.html.twig', [
+            'form' => $formTool->createView(),
+        ]);
+    }
 }

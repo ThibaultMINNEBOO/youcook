058c546a1a5f97d895cf9ff680fea8b1ffeba261
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-06 17:40:33 +0100

    feat(backoffice): develop attributes to expose on different point of view

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +17,17 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('lastname'),
+            TextField::new('firstname'),
+            TextField::new('email'),
+            TextField::new('picture')->setFormType(VichImageType::class)->onlyOnForms(),
+            ImageField::new('pictureName', 'Avatar')->setBasePath('/uploads/avatars/')->onlyOnIndex(),
+            TextEditorField::new('biography'),
         ];
     }
-    */
 }

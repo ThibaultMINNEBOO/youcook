8c0551229f4f4a079a10aa9f59fba3cb4e05f514
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-01-08 09:55:33 +0100

    correction cherry pick

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Constitute;
 use App\Entity\Recipe;
-use App\Entity\User;
 use App\Form\RecipeType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,23 +22,57 @@ class RecipeController extends AbstractController
     }
 
     #[Route('/recipe/{id}/create', name: 'app_recipe_create', requirements: ['id' => '\d+'])]
-    public function create(User $user, Request $request, EntityManagerInterface $entityManager): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // $userId = $this->getUser()->getUserIdentifier();
         $recipe = new Recipe();
-        $constitute = new Constitute();
-        $constitute->setRecipe($recipe);
-        $form = $this->createForm(RecipeType::class);
+        $form = $this->createForm(RecipeType::class, $recipe);
+
         $form->handleRequest($request);
 
-        foreach ($request->request->get('Ingredients') as $ingredient) {
-            $constitute->addIngredient($ingredient);
-        }
-        $recipe->setConstitute($constitute);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($recipe);
+            $finalRecipe = new Recipe();
+            foreach ($recipe->getConstitutes() as $ingredient) {
+                $constitute = new Constitute();
+                $constitute->setIngredient($ingredient);
+                $constitute->setRecipe($recipe->getId());
+                $constitute->setQuantity(1);
+                $constitute->setMeasure('kg');
+                $finalRecipe->addConstitute($constitute);
+            }
+            $finalRecipe->setRecipeCategory($recipe->getRecipeCategory());
+            $finalRecipe->setDescription($recipe->getDescription());
+            $finalRecipe->setMark($recipe->getMark());
+            $finalRecipe->setName($recipe->getName());
+            $finalRecipe->setTime($recipe->getTime());
+            $finalRecipe->setDifficulty($recipe->getDifficulty());
+            $finalRecipe->setNbPeople($recipe->getNbPeople());
+            $entityManager->persist($constitute);
+            $entityManager->persist($finalRecipe);
             $entityManager->flush();
+
+            // return $this->redirectToRoute("");
         }
+
+
+        /*if ($form->isSubmitted() && $form->isValid()) {
+            $ingredients = $request->request->get('constitutes');
+
+            $finalRecipe = $recipe;
+            foreach ($recipe->getConstitutes() as $ingredient) {
+                $constitute = new Constitute();
+                $constitute->setIngredient($ingredient);
+                $constitute->setRecipe($recipe->getId());
+                $constitute->setQuantity(1);
+                $constitute->setMeasure('kg');
+                $finalRecipe->addConstitute($constitute);
+            }
+
+            $entityManager->persist($finalRecipe);
+            $entityManager->flush();
+
+            // return $this->redirectToRoute("");
+        }*/
 
         return $this->render('recipe/create.html.twig', ['form' => $form->createView()]);
     }
--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -61,7 +61,7 @@ class Recipe
 
     #[ORM\OneToOne(mappedBy: 'recipe', cascade: ['persist', 'remove'])]
     private ?Constitute $constitute = null;
-    #[ORM\OneToMany(mappedBy: 'recipe', targetEntity: Constitute::class)]
+    #[ORM\OneToMany(mappedBy: 'recipes', targetEntity: Constitute::class, cascade: ['persist'])]
     private Collection $constitutes;
 
     public function __construct()

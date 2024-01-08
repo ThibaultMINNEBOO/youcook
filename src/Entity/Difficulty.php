eb093caf8ef2185757f6ac7d3d2c90d7f85fe37f
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-08 10:28:13 +0100

    fix: add values for difficulty enum

--- src/Entity/Difficulty.php
+++ src/Entity/Difficulty.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-enum Difficulty
+enum Difficulty: string
 {
-    case EASY;
-    case MEDIUM;
-    case HARD;
+    case EASY = 'Facile';
+    case MEDIUM = 'Moyen';
+    case HARD = 'Difficile';
 }

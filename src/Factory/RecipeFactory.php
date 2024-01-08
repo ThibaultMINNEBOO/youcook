ad8df719f88671aaf0490225c86545bc9e983712
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-08 10:28:31 +0100

    fix: add value instead of name for difficulty value

--- src/Factory/RecipeFactory.php
+++ src/Factory/RecipeFactory.php
@@ -59,7 +59,7 @@ final class RecipeFactory extends ModelFactory
          */
         return [
             'name' => self::faker()->text(100),
-            'difficulty' => Difficulty::cases()[array_rand(Difficulty::cases())]->name,
+            'difficulty' => Difficulty::cases()[array_rand(Difficulty::cases())]->value,
             'nbPeople' => self::faker()->numberBetween(1, 10),
             'mark' => MarkFactory::random(),
             'recipeCategory' => RecipesCategoryFactory::new(),

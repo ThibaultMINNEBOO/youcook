7a8a687be98bf75046f28a295276a3b1a5bccc94
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-01-08 15:12:19 +0100

    Ajout des recette dans l'action index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use App\Repository\RecipesCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(RecipesCategoryRepository $recipesCategoryRepository): Response
+    public function index(RecipesCategoryRepository $recipesCategoryRepository, RecipeRepository $recipeRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'categories' => $recipesCategoryRepository->findAll(),
+            'recipes' => $recipeRepository->findAll(),
         ]);
     }
 }

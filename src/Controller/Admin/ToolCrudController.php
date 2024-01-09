616ffaf70e7187dccaec09b80c25f6f58e2ad7e1
Thibault Minneboo <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-09 11:05:13 +0100

    feat(backoffice): configure tool fields

--- src/Controller/Admin/ToolCrudController.php
+++ src/Controller/Admin/ToolCrudController.php
@@ -15,14 +15,11 @@ class ToolCrudController extends AbstractCrudController
         return Tool::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
         ];
     }
-    */
 }

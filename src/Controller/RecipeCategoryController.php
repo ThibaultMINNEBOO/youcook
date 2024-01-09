8dcb494ead7c9cb67b371b8be442337b79aa1d32
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-01-09 11:46:51 +0100

    Ajout du controller RecipeCategory et ajout de l'action show

---
+++ src/Controller/RecipeCategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RecipesCategory;
+use App\Repository\RecipesCategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeCategoryController extends AbstractController
+{
+    #[Route('/recipe/category', name: 'app_recipe_category')]
+    public function index(): Response
+    {
+        return $this->render('recipe_category/index.html.twig');
+    }
+    #[Route('/recipe/category/{id}', name: 'app_recipe_category_show', requirements: ['id' => '\d+'])]
+    public function show(RecipesCategory $recipeCategory, RecipesCategoryRepository $recipesCategoryRepository): Response
+    {
+        return $this->render('recipe_category/show.html.twig', [
+            'recipeCategory' => $recipeCategory,
+            'categories' => $recipesCategoryRepository->findAll(),
+        ]);
+    }
+}

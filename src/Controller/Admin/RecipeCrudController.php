0cf2e10982e8d80fe27c009ffbcc70cac2184bef
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-09 06:13:22 +0100

    feat(backoffice): add category into recipe crud controller

--- src/Controller/Admin/RecipeCrudController.php
+++ src/Controller/Admin/RecipeCrudController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Difficulty;
 use App\Entity\Recipe;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
@@ -25,6 +27,7 @@ class RecipeCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name', 'Nom'),
+            TextareaField::new('description'),
             NumberField::new('day', 'Jour')->onlyOnForms()->setColumns(1),
             NumberField::new('hour', 'Heure')->onlyOnForms()->setColumns(1),
             NumberField::new('minute', 'Minute')->onlyOnForms()->setColumns(1),
@@ -33,9 +36,21 @@ class RecipeCrudController extends AbstractCrudController
                 'Moyen' => 'Moyen',
                 'Difficile' => 'Difficile',
             ])->onlyOnForms()->setColumns(10),
+            NumberField::new('nbPeople')->onlyOnForms(),
             TextField::new('difficulty')->hideOnForm(),
             TextField::new('picture', 'Image')->setFormType(VichImageType::class)->onlyOnForms(),
             ImageField::new('pictureName', 'Image')->setBasePath('/images/recipes')->onlyOnIndex(),
+            AssociationField::new('recipeCategory', 'Catégorie')
+                ->setFormTypeOptions([
+                    'choice_label' => 'name',
+                    'query_builder' => function ($recipeCategory) {
+                        return $recipeCategory->createQueryBuilder('r')
+                            ->orderBy('r.name', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getRecipeCategory()->getName();
+                }),
         ];
     }
 }

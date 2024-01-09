e95db2900b25915796413a51efeceb68dddc12a0
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2024-01-09 06:29:19 +0100

    feat(backoffice): add association of allergen and category

--- src/Controller/Admin/IngredientCrudController.php
+++ src/Controller/Admin/IngredientCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Ingredient;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +17,35 @@ class IngredientCrudController extends AbstractCrudController
         return Ingredient::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            ImageField::new('picture')->setBasePath('/icons/ingredients')->onlyOnIndex(),
+            AssociationField::new('category', 'Catégorie')
+                ->setFormTypeOptions([
+                    'choice_label' => 'name',
+                    'query_builder' => function ($category) {
+                        return $category->createQueryBuilder('c')
+                            ->orderBy('c.name', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getCategory()?->getName();
+                }),
+            AssociationField::new('allergen', 'Allergène')
+                ->setFormTypeOptions([
+                    'choice_label' => 'name',
+                    'query_builder' => function ($allergen) {
+                        return $allergen->createQueryBuilder('a')
+                            ->orderBy('a.name', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getAllergen()?->getName();
+                }),
         ];
     }
-    */
 }

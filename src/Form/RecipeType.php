5e5aa8df01388c19a1cb0df8324820d18cdb4ae3
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-01-09 10:43:01 +0100

    mise en place de l'ajout multiple d'ingredients avec boutons en JS

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -10,6 +10,7 @@ use App\Repository\ToolRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -45,25 +46,21 @@ class RecipeType extends AbstractType
                         ->orderBy('c.name', 'ASC');
                 },
             ])
-            ->add('constitutes', EntityType::class, [
-                'class' => Ingredient::class,
-                'multiple' => true,
-                'choice_label' => 'name',
-                'expanded' => true,
-                'attr' => [
-                    'class' => 'form-input',
-                ],
+
+            ->add('constitutes', CollectionType::class, [
+                'entry_type' => ConstituteType::class,
+                'label' => 'Ingredient',
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
             ])
-            ->add('quantity', TextType::class, [
-                'mapped' => false,
-            ])
+            ->add('valider', SubmitType::class)
 
             ->add('recipeCategory', EntityType::class, [
                 'class' => RecipesCategory::class,
                 'choice_label' => 'name',
             ])
-            ->add('save', SubmitType::class,
-                ['label' => 'Créer'])
         ;
     }
 

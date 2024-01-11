6e5909de7a8751a2e6e02d2c71d1bb8c9d8f8104
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-01-11 15:12:34 +0100

    ajout de la migration

--- migrations/Version20240109091247.php
+++ migrations/Version20240109091247.php
@@ -20,12 +20,10 @@ final class Version20240109091247 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE constitute ADD recipe_id INT DEFAULT NULL, CHANGE measure measure VARCHAR(10) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE constitute DROP recipe_id, CHANGE measure measure VARCHAR(15) NOT NULL');
     }
 }

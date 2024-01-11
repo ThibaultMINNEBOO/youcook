0c1c99a274806425d6d4c0ee0b4be3bdd95b2cd3
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-01-11 08:54:49 +0100

    Ajout de la selection des categorie de recette a l'action index

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -31,6 +31,7 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/index.html.twig', [
+            'categories' => $recipesCategoryRepository->findAll(),
             'form' => $form->createView(),
             'categories' => $recipesCategoryRepository->findAll(),
             'users' => $userRepository->findAll(),

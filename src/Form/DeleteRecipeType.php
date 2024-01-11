d94ba663a8a97100ae1c795081e4a97df7a70de9
Dylan Huckel <dylan.huckel@etudiant.univ-reims.fr>
2024-01-11 11:22:14 +0100

    construction du bouton de suppression

--- src/Form/DeleteRecipeType.php
+++ src/Form/DeleteRecipeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,20 +13,10 @@ class DeleteRecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('difficulty')
-            ->add('description')
-            ->add('nbPeople')
-            ->add('day')
-            ->add('hour')
-            ->add('minute')
-            ->add('pictureName')
-            ->add('updatedAt')
-            ->add('mark')
-            ->add('tools')
-            ->add('recipeCategory')
-            ->add('constitute')
-        ;
+            ->add('confirm_delete', SubmitType::class, [
+                'label' => 'Confirmer la suppression',
+                'attr' => ['class' => 'btn btn-danger'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

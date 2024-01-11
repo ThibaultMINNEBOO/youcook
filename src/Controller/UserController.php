f8f209034392cdb9d11479c78a21bc162dd2c73c
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-01-11 23:33:31 +0100

    Suppression appel doublon de parametre

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -31,7 +31,6 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/index.html.twig', [
-            'categories' => $recipesCategoryRepository->findAll(),
             'form' => $form->createView(),
             'categories' => $recipesCategoryRepository->findAll(),
         ]);

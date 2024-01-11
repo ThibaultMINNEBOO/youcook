26528100821db01c8d16b091fe4de56fb8944ce1
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-01-11 15:11:22 +0100

    suppression d'éléments inutiles dans la fixture de user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\RecipeFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,7 +12,6 @@ class UserFixtures extends Fixture
     {
         UserFactory::createOne([
             'firstname' => 'nassimUser',
-            'recipes' => RecipeFactory::createMany(10),
         ]);
         UserFactory::createOne(['email' => 'root@example.com', 'firstname' => 'Thibault', 'lastname' => 'Minneboo', 'roles' => ['ROLE_ADMIN']]);
         UserFactory::createMany(20);

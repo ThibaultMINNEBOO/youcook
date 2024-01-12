9172d8d9fafa08deb4122f74fb31b8d75e951245
Nassim MEKHALDI <nassim.mekhaldi@etudiant.univ-reims.fr>
2024-01-12 02:50:21 +0100

    Ajout de l'action create dans IngredientController

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
 use App\Repository\RecipesCategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class IngredientController extends AbstractController
 {
@@ -26,5 +31,26 @@ class IngredientController extends AbstractController
     public function show(Ingredient $ingredient, RecipesCategoryRepository $recipesCategoryRepository): Response
     {
         return $this->render('ingredient/show.html.twig', ['ingredientId' => $ingredient->getId(), 'ingredient' => $ingredient, 'categories' => $recipesCategoryRepository->findAll()]);
+    }
+
+    #[Route('/ingredient/create', name: 'app_ingredient_create')]
+    #[IsGranted('IS_AUTHENTICATED')]
+    public function create(EntityManagerInterface $entityManager, Request $request, RecipesCategoryRepository $recipesCategoryRepository): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_ingredient_show', ['id' => $ingredient->getId()], 301);
+        }
+
+
+        return $this->render('ingredient/create.html.twig', [
+            'form' => $form->createView(),
+            'categories' => $recipesCategoryRepository->findAll(),
+        ]);
     }
 }
